67071ac3ece4994c8d1a3a065b23d524511ba99e
CooperHeriot <chepic9990@gmail.com>
2023-03-15 00:52:34 +1100

    ds

---
+++ Assets/Prototype2/Scripts/Player.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject ring, clickPoint, currentPoint;
+    private SelectionRing selRing;
+
+    public Vector3 ringPos;
+
+    public bool dragging;
+    // Start is called before the first frame update
+    void Start()
+    {
+        selRing = ring.GetComponent<SelectionRing>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ringPos = ((clickPoint.transform.position + currentPoint.transform.position) / 2);
+
+        if (Input.GetMouseButton(0)){
+            dragging = true;
+        } else
+        {
+            dragging = false;
+        }
+
+
+        if (dragging == true)
+        {
+            ring.SetActive(true);
+            ring.transform.position = ringPos;
+        } else
+        {
+            ring.SetActive(false);
+        }
+    }
+}
