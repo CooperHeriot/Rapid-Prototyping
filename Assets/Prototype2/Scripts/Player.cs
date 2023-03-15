afe3e00eeb34583c41b02b9c172b439fe697a8bc
CooperHeriot <107088336+CooperHeriot@users.noreply.github.com>
2023-03-15 13:32:32 +1100

    ghc

--- Assets/Prototype2/Scripts/Player.cs
+++ Assets/Prototype2/Scripts/Player.cs
@@ -10,6 +10,11 @@ public class Player : MonoBehaviour
     public Vector3 ringPos;
 
     public bool dragging;
+
+    //mouse stuff
+    public Camera cam;
+    public Vector3 screenPos, worldPosition;
+    public float dist;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +24,14 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        screenPos = Input.mousePosition;
+        screenPos.z = dist;
+
+        worldPosition = cam.ScreenToWorldPoint(screenPos);
+
+        currentPoint.transform.position = worldPosition;
+
+
         ringPos = ((clickPoint.transform.position + currentPoint.transform.position) / 2);
 
         if (Input.GetMouseButton(0)){
@@ -34,6 +47,8 @@ public class Player : MonoBehaviour
             ring.SetActive(true);
             ring.transform.position = ringPos;
             selRing.ChangeSize(Vector3.Distance(clickPoint.transform.position, currentPoint.transform.position));
+
+
         } else
         {
             ring.SetActive(false);
