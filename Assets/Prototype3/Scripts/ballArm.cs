5dfdbe13bd5ee3b58b27773382fc757ed8396fae
CooperHeriot <107088336+CooperHeriot@users.noreply.github.com>
2023-03-22 18:47:41 +1100

    p3

---
+++ Assets/Prototype3/Scripts/ballArm.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballArm : MonoBehaviour
+{
+    public HingeJoint HJ1, HJ2;
+
+    public float Spring1, spring2;
+    // Start is called before the first frame update
+    void Start()
+    {
+       // HJ1 = GetComponent<HingeJoint>();
+       // HJ2 = GetComponent<HingeJoint>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Spring1 = HJ1.spring.targetPosition;
+
+        //HJ1.spring.targetPosition = Spring1;
+
+        
+    }
+}
